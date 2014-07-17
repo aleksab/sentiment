ff1604ddf897995b26f39e613e1bc94279a211f1
Alex <ab@prognett.no>
2014-07-17 05:01:54 +0200

    Adjusted to better connection pool library

--- src/main/java/no/hioa/sentiment/util/DatabaseUtilities.java
+++ src/main/java/no/hioa/sentiment/util/DatabaseUtilities.java
@@ -3,26 +3,26 @@ package no.hioa.sentiment.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 public class DatabaseUtilities
 {
 	public static JdbcTemplate getMySqlTemplate(String url, String database, String username, String password) throws UnsupportedEncodingException
 	{
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl(DatabaseUtilities.getMySqlConnectionString(url, database, username, password));
-
+		
 		return new JdbcTemplate(dataSource);
 	}
 
 	public static JdbcTemplate getMsSqlTemplate(String url, String username, String password)
 	{
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("net.sourceforge.jtds.jdbc.Driver");
 		dataSource.setUrl(DatabaseUtilities.getMsSqlConnectionString(url, username, password));
-
+		
 		return new JdbcTemplate(dataSource);
 	}
 
